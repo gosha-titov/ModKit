8e1dccf747946b6eae3a249f0c776f5ef08f9447
gosha <gosha.titov@icloud.com>
2022-11-22 19:28:24 +0300

    Feat: add digitCount, digits and abs properties.

---
+++ Sources/ModKit/StandartLibrary/SignedInteger+.swift
@@ -0,0 +1,28 @@
+public extension SignedInteger {
+    
+    /// An `Int` value that is the number of digits of this instance.
+    ///
+    ///     let number = -592
+    ///     number.digitCount // 3
+    ///
+    var digitCount: Int {
+        return String(abs).count
+    }
+
+    /// An array containing the digits of this instance.
+    ///
+    ///     let number = -312
+    ///     number.digits // [3, 1, 2]
+    ///
+    var digits: [Int] {
+        return String(abs).map { $0.toInt! }
+    }
+
+    /// The absolute value of this instance.
+    ///
+    ///     let number = -30
+    ///     number.abs // 30
+    ///
+    var abs: Self { Swift.abs(self) }
+    
+}
