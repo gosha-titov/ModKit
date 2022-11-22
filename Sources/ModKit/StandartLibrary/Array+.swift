9f6a974ea6169b983c392cab9bd922ec531b05ae
gosha <gosha.titov@icloud.com>
2022-11-22 18:55:57 +0300

    Feat: add removed(_:), remove(_:) and removedDuplicates(), removeDuplicate() methods.

--- Sources/ModKit/StandartLibrary/Array+.swift
+++ Sources/ModKit/StandartLibrary/Array+.swift
@@ -50,3 +50,46 @@ public extension Array {
     }
     
 }
+
+
+public extension Array where Element: Equatable {
+    
+    /// Returns an array containing all but the given elements.
+    ///
+    ///     let array = [1, 2, 3, 2, 4]
+    ///     array.removed([2, 4]) // [1, 3]
+    ///
+    func removed(_ elements: [Element]) -> [Element] {
+        return filter { !elements.contains($0) }
+    }
+    
+    /// Removes the given elements from the array.
+    ///
+    ///     var array = [1, 2, 3, 2, 4]
+    ///     array.remove([2, 4]) // [1, 3]
+    ///
+    mutating func remove(_ elements: [Element]) -> Void {
+        self = removed(elements)
+    }
+    
+    /// Returns an array containing all but duplicates, leaving only the first element of them.
+    ///
+    ///     let array = [1, 2, 3, 2, 4, 4, 5, 4]
+    ///     array.removedDuplicates() // [1, 2, 3, 4, 5]
+    ///
+    func removedDuplicates() -> [Element] {
+        var result = [Element]()
+        forEach { if !result.contains($0) { result.append($0) } }
+        return result
+    }
+    
+    /// Removes all duplicate elements, leaving only the first element of them.
+    ///
+    ///     var array = [1, 2, 3, 2, 4, 4, 5, 4]
+    ///     array.removeDuplicates() // [1, 2, 3, 4, 5]
+    ///
+    mutating func removeDuplicates() -> Void {
+        self = removedDuplicates()
+    }
+    
+}
