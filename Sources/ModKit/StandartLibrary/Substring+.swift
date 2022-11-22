340e42e01190476740960f7c80b95de7c05ecc9a
gosha <gosha.titov@icloud.com>
2022-11-22 20:06:51 +0300

    Feat: add toString property.

---
+++ Sources/ModKit/StandartLibrary/Substring+.swift
@@ -0,0 +1,10 @@
+public extension Substring {
+    
+    /// A `String` value converted from this substring.
+    ///
+    ///     let sub = "12345".dropLast(2) // "45" is substring
+    ///     let str = sub.toString // "45" is string
+    ///
+    var toString: String { String(self) }
+    
+}
