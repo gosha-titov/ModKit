8e0962acb57cc82ecb7eed864c076802096c1ca7
gosha <gosha.titov@icloud.com>
2023-07-25 16:36:24 +0300

    Feat: add `asArray` properties

--- Sources/ModKit/StandartLibrary/Dictionary+.swift
+++ Sources/ModKit/StandartLibrary/Dictionary+.swift
@@ -40,3 +40,37 @@ public extension Dictionary where Value: AnyObject {
     }
     
 }
+
+
+public extension Dictionary.Keys {
+    
+    /// Returns an array consisting of keys.
+    ///
+    /// Unfortunately, you can't get the array of keys directly.
+    /// Therefore, we have to use such "hacks":
+    ///
+    ///     let dict = ["a": 1, "b": 2]
+    ///     let keys: [String] = dict.keys.asArray // ["a", "b"]
+    ///
+    var asArray: [Key] {
+        return Array(self)
+    }
+    
+}
+
+
+public extension Dictionary.Values {
+    
+    /// Returns an array consisting of values.
+    ///
+    /// Unfortunately, you can't get the array of values directly.
+    /// Therefore, we have to use such "hacks":
+    ///
+    ///     let dict = ["a": 1, "b": 2]
+    ///     let values: [Int] = dict.values.asArray // [1, 2]
+    ///
+    var asArray: [Value] {
+        return Array(self)
+    }
+    
+}
