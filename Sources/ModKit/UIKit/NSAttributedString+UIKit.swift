9279539fdbd193f55e4c1211ed4b0222d7fc71e3
gosha <gosha.titov@icloud.com>
2023-08-03 06:59:19 +0300

    Feat: add the `applying(backgroundColor:inRange)` method

--- Sources/ModKit/UIKit/NSAttributedString+UIKit.swift
+++ Sources/ModKit/UIKit/NSAttributedString+UIKit.swift
@@ -6,10 +6,18 @@ public extension NSAttributedString {
     /// Returns an attributed string with applied an underline attribute with the given color and style in the specified range.
     /// - Parameter color: The color of the underline.
     /// - Parameter style: The underline style of the text.
-    /// - Parameter range: The range of characters to which the specified attributes apply.
-    /// If `nil` is specified, the given attributes are applied to the full string.
+    /// - Parameter range: The range of characters to which these attributes apply.
+    /// If `nil` is specified,  these attributes are applied to the full string.
     func underlined(withColor color: UIColor, andStyle style: NSUnderlineStyle = .single, inRange range: NSRange? = nil) -> NSAttributedString {
         return applying([.underlineColor: color, .underlineStyle: style.rawValue], inRange: range)
+    }
+    
+    /// Returns an attributed string with applied a background attribute with the given color in the specified range.
+    /// - Parameter color: The color of the background behind the text.
+    /// - Parameter range: The range of characters to which this attribute applies.
+    /// If `nil` is specified, this attribute is applied to the full string.
+    func applying(backgroundColor: UIColor, inRange range: NSRange? = nil) -> NSAttributedString {
+        return applying([.backgroundColor: backgroundColor], inRange: range)
     }
     
 }
