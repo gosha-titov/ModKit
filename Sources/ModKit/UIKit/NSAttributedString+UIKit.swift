6345307852a7d229e55bfa7a44248edbdff7ea32
gosha <gosha.titov@icloud.com>
2023-08-03 07:17:19 +0300

    Feat: add the `applying(font:inRange:)` method

--- Sources/ModKit/UIKit/NSAttributedString+UIKit.swift
+++ Sources/ModKit/UIKit/NSAttributedString+UIKit.swift
@@ -1,15 +1,24 @@
 #if canImport(UIKit)
+
 import UIKit
 
 public extension NSAttributedString {
     
-    /// Returns an attributed string with applied an underline attribute with the given color and style in the specified range.
-    /// - Parameter color: The color of the underline.
+    /// Returns an attributed string with applied an underline attribute with the given style and color in the specified range.
     /// - Parameter style: The underline style of the text.
+    /// - Parameter color: The color of the underline.
     /// - Parameter range: The range of characters to which these attributes apply.
     /// If `nil` is specified,  these attributes are applied to the full string.
-    func underlined(withColor color: UIColor, andStyle style: NSUnderlineStyle = .single, inRange range: NSRange? = nil) -> NSAttributedString {
+    func applying(underline style: NSUnderlineStyle = .single, withColor color: UIColor, inRange range: NSRange? = nil) -> NSAttributedString {
         return applying([.underlineColor: color, .underlineStyle: style.rawValue], inRange: range)
+    }
+    
+    /// Returns an attributed string with applied a font attribute in the specified range.
+    /// - Parameter font: The font of the text.
+    /// - Parameter range: The range of characters to which this attribute applies.
+    /// If `nil` is specified, this attribute is applied to the full string.
+    func applying(font: UIFont, inRange range: NSRange? = nil) -> NSAttributedString {
+        return applying([.font: font], inRange: range)
     }
     
     /// Returns an attributed string with applied a background attribute with the given color in the specified range.
