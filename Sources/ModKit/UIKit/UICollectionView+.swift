6f6f20c2da1d0875bc5c375bbb8e9502d95f954e
gosha <gosha.titov@icloud.com>
2023-09-06 21:53:52 +0300

    Refactor: move scroll methods

--- Sources/ModKit/UIKit/UICollectionView+.swift
+++ Sources/ModKit/UIKit/UICollectionView+.swift
@@ -4,23 +4,6 @@ import UIKit
 
 public extension UICollectionView {
     
-    /// Scrolls to top by setting the content offset to zero.
-    /// - Parameter animated: Specify `true` to animate scroll transition, or `false` if you do not want the scroll transition to be animated.
-    /// The default value is `true`.
-    func scrollToTop(animated: Bool = true) -> Void {
-        setContentOffset(.zero, animated: animated)
-    }
-    
-    /// Scrolls to bottom by setting the content offset to the bottom point.
-    /// - Parameter animated: Specify `true` to animate scroll transition, or `false` if you do not want the scroll transition to be animated.
-    /// The default value is `true`.
-    func scrollToBottom(animated: Bool = true) -> Void {
-        let yOffset: CGFloat = contentSize.height - bounds.size.height
-        guard yOffset > 0 else { return }
-        let offset = CGPoint(x: 0, y: yOffset)
-        setContentOffset(offset, animated: animated)
-    }
-    
     /// Registers a class for use in creating new collection view cells.
     ///
     /// The method registers a cell of the given type using its reuse identifier.
