a52c2b6e9967df37a11504ea72a718c425cebcdc
gosha <gosha.titov@icloud.com>
2023-09-26 19:37:47 +0300

    Docs: update the description

--- Sources/ModKit/StandartLibrary/Substring+.swift
+++ Sources/ModKit/StandartLibrary/Substring+.swift
@@ -2,8 +2,8 @@ public extension Substring {
     
     /// A String value converted from this substring.
     ///
-    ///     let sub = "12345".dropLast(2) // "45" is substring
-    ///     let str = sub.toString // "45" is string
+    ///     let sub = "12345".dropLast(2) // "123" is substring
+    ///     let str = sub.toString // "123" is string
     ///
     var toString: String { String(self) }
     
