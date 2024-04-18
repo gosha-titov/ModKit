67496bbe2d7c326653521e3e70922c95cebe7c6b
gosha <gosha.titov@icloud.com>
2024-04-19 01:11:57 +0300

    Feat: add `inserting(_:)` methods

--- Sources/StandartLibrary/Set+.swift
+++ Sources/StandartLibrary/Set+.swift
@@ -8,4 +8,30 @@ public extension Set {
     ///
     @inlinable static var empty: Self { [] }
     
+    /// Returns a new set with the specified element inserted.
+    ///
+    ///     let set: Set<Int> = [0, 1]
+    ///     print(set.inserting(2))
+    ///     // Prints [0, 1, 2]
+    ///
+    @inlinable func inserting(_ element: Element) -> Self {
+        var mutableSelf = self
+        mutableSelf.insert(element)
+        return mutableSelf
+    }
+    
+    /// Returns a new set with the specified elements inserted.
+    ///
+    ///     let set: Set<Int> = [0, 1]
+    ///     print(set.inserting([2, 3]))
+    ///     // Prints [0, 1, 2, 3]
+    ///
+    @inlinable func inserting(_ elements: [Element]) -> Self {
+        var mutableSelf = self
+        for element in elements {
+            mutableSelf.insert(element)
+        }
+        return mutableSelf
+    }
+    
 }
