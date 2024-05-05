6b115a2dd93b90cbd9b553f61a653e57b9f7d39c
gosha <gosha.titov@icloud.com>
2024-05-05 22:00:12 +0300

    Feat: add the `filled(withColor:size:)` static method

--- Sources/UIKit/UIImage+.swift
+++ Sources/UIKit/UIImage+.swift
@@ -34,6 +34,20 @@ public extension UIImage {
         return size.height / size.width
     }
     
+    
+    // MARK: Methods
+    
+    /// Returns a created image fully filled with the specified color.
+    ///
+    ///     let image: UIImage = .filled(withColor: .red)
+    ///
+    @inlinable static func filled(withColor color: UIColor, size: CGSize = CGSize(side: 1)) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { rendererContext in
+            color.setFill()
+            rendererContext.fill(CGRect(origin: .zero, size: size))
+        }
+    }
+    
 }
 
 #endif
