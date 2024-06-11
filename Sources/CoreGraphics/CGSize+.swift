6971bbe89f21b6d722b892363c270bf1628fa45a
gosha <gosha.titov@icloud.com>
2024-06-12 02:55:38 +0300

    Feat: add `widthToHeightRatio` and `heightToWidthRatio` properties

--- Sources/CoreGraphics/CGSize+.swift
+++ Sources/CoreGraphics/CGSize+.swift
@@ -2,23 +2,61 @@ import CoreGraphics
 
 public extension CGSize {
     
-    /// Max dimension between width and height.
-    @inlinable var maxDimension: CGFloat { max(width, height) }
+    /// Maximum dimension between this size width and its height.
+    ///
+    ///     size.width // 200
+    ///     size.height // 50
+    ///     size.maximumDimension // 200
+    ///
+    @inlinable var maximumDimension: CGFloat { max(width, height) }
     
-    /// Min dimension between width and height.
-    @inlinable var minDimension: CGFloat { min(width, height) }
+    /// Minimum dimension between this size width and its height.
+    ///
+    ///     size.width // 200
+    ///     size.height // 50
+    ///     size.minimumDimension // 50
+    ///
+    @inlinable var minimumDimension: CGFloat { min(width, height) }
+    
+    /// Returns a value corresponding to the ratio of this size width to its height.
+    ///
+    ///     size // CGSize(width: 200, height: 100)
+    ///     size.widthToHeightRatio // 2.0
+    ///
+    ///     // Usage example:
+    ///     view.widthAnchor.constraint(
+    ///         equalTo: view.heightAnchor,
+    ///         multiplier: size.widthToHeightRatio
+    ///     )
+    @inlinable var widthToHeightRatio: CGFloat {
+        return width / height
+    }
+    
+    /// Returns a value corresponding to the ratio of this size height to its width.
+    ///
+    ///     size // CGSize(width: 200, height: 100)
+    ///     size.heightToWidthRatio // 0.5
+    ///
+    ///     // Usage example:
+    ///     view.heightAnchor.constraint(
+    ///         equalTo: view.widthAnchor,
+    ///         multiplier: size.heightToWidthRatio
+    ///     )
+    @inlinable var heightToWidthRatio: CGFloat {
+        return height / width
+    }
     
     
     // MARK: Init
     
-    /// Creates a size with the dimension specified as CGFloat value.
+    /// Creates a size with the specified dimension for the width and height properties.
     ///
-    ///     let size = CGSize(side: 100.0)
+    ///     let size = CGSize(dimension: 100.0)
     ///     size.width  // 100.0
     ///     size.height // 100.0
     ///
-    @inlinable init(side: CGFloat) {
-        self.init(width: side, height: side)
+    @inlinable init(dimension: CGFloat) {
+        self.init(width: dimension, height: dimension)
     }
     
     
