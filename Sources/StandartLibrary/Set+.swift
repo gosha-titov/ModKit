e246c95d807d1597a203a9d0396af0cfece82302
gosha <gosha.titov@icloud.com>
2024-07-01 19:02:22 +0300

    Feat: add `remove(_:)` and `removing(_:)` methods

--- Sources/StandartLibrary/Set+.swift
+++ Sources/StandartLibrary/Set+.swift
@@ -30,4 +30,37 @@ public extension Set {
         return union(elements)
     }
     
+    /// Removes the specified elements from the set.
+    ///
+    ///     var set: Set<Int> = [0, 1, 2, 3]
+    ///     set.remove([2, 3])
+    ///     print(set)
+    ///     // Prints [0, 1]
+    ///
+    @inlinable mutating func remove(_ elements: [Element]) -> Void {
+        self = subtracting(elements)
+    }
+    
+    /// Returns a new set with the specified elements removed.
+    ///
+    ///     var set: Set<Int> = [0, 1, 2, 3]
+    ///     print(set.removing([2, 3]))
+    ///     // Prints [0, 1]
+    ///
+    @inlinable func removing(_ elements: [Element]) -> Self {
+        return subtracting(elements)
+    }
+    
+    /// Returns a new set with the specified element removed.
+    ///
+    ///     var set: Set<Int> = [0, 1, 2, 3]
+    ///     print(set.removing(2))
+    ///     // Prints [0, 1, 3]
+    ///
+    @inlinable func removing(_ element: Element) -> Self {
+        var mutableSelf = self
+        mutableSelf.remove(element)
+        return mutableSelf
+    }
+    
 }
