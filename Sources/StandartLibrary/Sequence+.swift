9261c766846ed9e7f046ee0e15722888bfb7ba18
gosha <gosha.titov@icloud.com>
2025-01-04 05:17:47 +0300

    Feat: add `forEach(where:body:)` and `first(excluding:)` methods

--- Sources/StandartLibrary/Sequence+.swift
+++ Sources/StandartLibrary/Sequence+.swift
@@ -1,3 +1,20 @@
+public extension Sequence {
+    
+    /// Calls the specified closure on each element satisfying the given predicate in the sequence in the same order as a for-in-where loop.
+    ///
+    ///     let array = [0, 1, 2, 3, 4, 5]
+    ///     array.forEach(where: { $0 % 2 == 0 }) {
+    ///         print($0)
+    ///     )
+    ///     // Prints "0, 2, 4"
+    ///
+    @inlinable func forEach(where condition: (Element) throws -> Bool, body: (Element) throws -> Void) rethrows {
+        try lazy.filter(condition).forEach(body)
+    }
+    
+}
+
+
 public extension Sequence where Element: Comparable {
     
     /// Returns the first element of the sequence except the specified ones; otherwise, `nil` if there is no available element.
