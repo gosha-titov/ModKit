9d0791389026ae1a5c3fce8ea7b0b9a736e508b2
gosha <gosha.titov@icloud.com>
2025-01-11 00:45:52 +0300

    Feat: add `except(where:)` method

--- Sources/StandartLibrary/Sequence+.swift
+++ Sources/StandartLibrary/Sequence+.swift
@@ -1,6 +1,6 @@
 public extension Sequence {
     
-    /// Calls the specified closure on each element satisfying the given predicate in the sequence in the same order as a for-in-where loop.
+    /// Calls the specified closure on each element that satisfies the given predicate in the sequence in the same order as a `for-in-where` loop.
     ///
     ///     let array = [0, 1, 2, 3, 4, 5]
     ///     array.forEach(where: { $0 % 2 == 0 }) {
@@ -10,6 +10,23 @@ public extension Sequence {
     ///
     @inlinable func forEach(where isIncluded: (Element) throws -> Bool, body: (Element) throws -> Void) rethrows {
         try lazy.filter(isIncluded).forEach(body)
+    }
+    
+    /// Returns an array containing, in order, the elements of the sequence except for the ones that satisfy the given predicate.
+    ///
+    /// This method is mainly used in cases when you want to specify the key path but you can't use it with the `!` unary operator.
+    ///
+    ///     let sourceNames = ["Mia", "Emma", "", "Luna"]
+    ///     let names = sourceNames.except(where: \.isEmpty)
+    ///     // ["Mia", "Emma", "Luna"]
+    ///
+    ///     // This is the same as the above
+    ///     let names = sourceNames.filter { !$0.isEmpty }
+    ///
+    /// - Parameter isNotIncluded: A closure that takes an element of the sequence as its argument
+    ///   and returns a Boolean value indicating whether the element should not be included in the returned array.
+    @inlinable func except(where isNotIncluded: (Element) throws -> Bool) rethrows -> [Element] {
+        return try lazy.filter { try !isNotIncluded($0) }
     }
     
 }
