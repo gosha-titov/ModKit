82135e5cb50e5ac443a5d5b7e425f6f245af52f1
gosha <gosha.titov@icloud.com>
2025-02-19 14:41:25 +0300

    Feat: add `decoded(using:)` and `encoded(using:)` methods

--- Sources/Foundation/Data+.swift
+++ Sources/Foundation/Data+.swift
@@ -37,6 +37,37 @@ public extension Data {
         return try decoder.decode(T.self, from: self)
     }
     
+    
+    /// Returns a decoded top-level value of the given type from the given property list representation.
+    ///
+    ///     // Usage example:
+    ///     let configuration = try file.read()
+    ///         .decoded(Configuration.self, using: decoder)
+    ///
+    /// - Parameter type: The type of the value to decode.
+    /// - Parameter decoder: The object to decode this data.
+    /// - Returns: A value of the requested type.
+    /// - Throws: `DecodingError.dataCorrupted` if values requested from the payload are corrupted, or if the given data is not a valid property list.
+    /// - Throws: An error if any value throws an error during decoding.
+    @inlinable @inline(__always)
+    func decoded<T: Decodable>(_ type: T.Type, using decoder: PropertyListDecoder) throws -> T {
+        return try decoder.decode(T.self, from: self)
+    }
+    
+    /// Returns a decoded top-level value of the given type from the given property list representation.
+    ///
+    ///     // Usage example:
+    ///     self.configuration = try file.read()
+    ///         .decoded(using: decoder)
+    ///
+    /// - Parameter decoder: The object to decode this data.
+    /// - Returns: A value of the requested type.
+    /// - Throws: `DecodingError.dataCorrupted` if values requested from the payload are corrupted, or if the given data is not a valid property list.
+    @inlinable @inline(__always)
+    func decoded<T: Decodable>(using decoder: PropertyListDecoder) throws -> T {
+        return try decoder.decode(T.self, from: self)
+    }
+    
 }
 
 #endif
--- Sources/Foundation/Encodable+.swift
+++ Sources/Foundation/Encodable+.swift
@@ -7,16 +7,30 @@ public extension Encodable {
     /// Returns a JSON-encoded representation of this value.
     ///
     ///     // Usage example:
-    ///     userDefaults.standard.set(
-    ///         try configuration.encoded(using: encoder),
-    ///         forKey: Keys.configuration
-    ///     )
+    ///     let data = configuration
+    ///         .adding(options)
+    ///         .encoded(using: encoder)
     ///
     /// - Returns: A new `Data` value containing the encoded JSON data.
     /// - Throws: `EncodingError.invalidValue` if a non-conforming floating-point value is encountered during encoding, and the encoding strategy is `.throw`.
     /// - Throws: An error if any value throws an error during encoding.
     @inlinable @inline(__always)
     func encoded(using encoder: JSONEncoder) throws -> Data {
+        return try encoder.encode(self)
+    }
+    
+    /// Returns a property list that represents an encoded version of the value you supply.
+    ///
+    ///     // Usage example:
+    ///     let data = configuration
+    ///         .adding(options)
+    ///         .encoded(using: encoder)
+    ///
+    /// - Returns: A new `Data` value containing the encoded JSON data.
+    /// - Throws: `EncodingError.invalidValue` if a non-conforming floating-point value is encountered during encoding, and the encoding strategy is `.throw`.
+    /// - Throws: An error if any value throws an error during encoding.
+    @inlinable @inline(__always)
+    func encoded(using encoder: PropertyListEncoder) throws -> Data {
         return try encoder.encode(self)
     }
     
