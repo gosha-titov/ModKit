ad030c7fb56f51b903e5ce9b149aeeecc2673d6a
gosha <gosha.titov@icloud.com>
2025-06-23 16:00:52 +0300

    Feat: add `vertical` and `horizontal` properties, add initializers

---
+++ Sources/UIKit/NSDirectionalEdgeInsets+.swift
@@ -0,0 +1,56 @@
+#if canImport(UIKit)
+
+import UIKit
+
+extension NSDirectionalEdgeInsets {
+    
+    /// Returns a sum of the top and bottom edge insets' values.
+    ///
+    ///     let availableHeight = bounds.height - padding.vertical
+    ///
+    ///     // The same as above:
+    ///     let availableHeight = bounds.height - (padding.top + padding.bottom)
+    ///
+    @inlinable @inline(__always)
+    var vertical: CGFloat {
+        return top + bottom
+    }
+    
+    /// Returns a sum of the leading and trailing edge insets' values.
+    ///
+    ///     let availableWidth = bounds.width - padding.horizontal
+    ///
+    ///     // The same as above:
+    ///     let availableWidth = bounds.width - (padding.leading + padding.trailing)
+    ///
+    @inlinable @inline(__always)
+    var horizontal: CGFloat {
+        return leading + trailing
+    }
+    
+    
+    // MARK: Init
+    
+    /// Creates an edge insets structure with the specified paired edges.
+    ///
+    ///     let padding = NSDirectionalEdgeInsets(vertical: 16, horizontal: 8)
+    ///     // NSDirectionalEdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8)
+    ///
+    @inlinable @inline(__always)
+    init(vertical: CGFloat, horizontal: CGFloat) {
+        self.init(top: vertical, leading: horizontal, bottom: vertical, trailing: horizontal)
+    }
+    
+    /// Creates an edge insets structure with the specified common inset.
+    ///
+    ///     let padding = NSDirectionalEdgeInsets(inset: 8)
+    ///     // NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+    ///
+    @inlinable @inline(__always)
+    init(inset: CGFloat) {
+        self.init(top: inset, leading: inset, bottom: inset, trailing: inset)
+    }
+    
+}
+
+#endif
